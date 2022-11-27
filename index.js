022c7adfff921434d7c6de28f976abba5ba0b277
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-28 02:21:01 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+let destructivelyAppendCat = () => cats.push('Ralph');
+
+let destructivelyPrependCat = () => cats.unshift('Bob');
+
+let destructivelyRemoveFirstCat = () => cats.shift('Broom');
+
+let destructivelyRemoveLastCat = () => cats.pop();
+
+function appendCat(){
+    const appendCat2 = cats.slice()
+    appendCat2.push()
+    return appendCat2;
+}
+
+function prependCat(){
+    const prependCat2 = cats.slice()
+    prependCat2.unshift()
+    return prependCat2;
+}
+
+
+function removeLastCat(){
+    const removeLast = cats.slice()
+    removeLast.pop();
+    return removeLast;
+}
+
+
+function removeFirstCat(){
+    const removeFirst = cats.slice()
+    removeFirst.shift();
+    return removeFirst;
+}
